584a38678f635530877b631a81c1ef85738696f1
Denis Wolf <dear.mr.dw@gmail.com>
2013-02-06 23:03:28 +0200

    JS: wrappers for console messages

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,5 +1,26 @@
 (function($){
     $('document').ready(function(){
     // javascript for all pages
+
+    //  PRETTY PRINT
+    //  logging functions wrapped for use in dev mode only
+        window.c = {};
+
+        // show state of loaded components
+        window.c.loaded = function logToConsole(){
+            console.log.apply(console, Array.prototype.slice.call(arguments));
+        };
+
+        // print detailed info on arguments
+        window.c.pp = function prettyPrint(){
+            console.dir.apply(console, arguments);
+        };
+
+        // we all love old console.log
+        window.c.log = function consoleLog(){
+            console.log.apply(console, arguments);
+        };
+
+
     });
 })(jQuery);

42c2d992be992c7d85dd7355414c3e41a4151758
Denis Wolf <dear.mr.dw@gmail.com>
2013-02-10 23:16:26 +0200

    LOGs sugar loaded before dom ready

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,30 +1,29 @@
 (function($){
-    $('document').ready(function(){
     // javascript for all pages
 
     //  PRETTY PRINT
     //  logging functions wrapped for use in dev mode only
-        window.c = {};
+    window.c = {};
 
-        // show state of loaded components
-        window.c.loaded = function logToConsole(){
-            var passedMessage = Array.prototype.slice.call(arguments).join(' , '),
-                message = "LOADED: "+moment().format('H:mm:ss') + ': '+passedMessage;
-            console.log.call(console, message);
-        };
+    // show state of loaded components
+    window.c.loaded = function logToConsole(){
+        var passedMessage = Array.prototype.slice.call(arguments).join(' , '),
+            message = "LOADED: "+moment().format('H:mm:ss') + ': '+passedMessage;
+        console.log.call(console, message);
+    };
 
-        // print detailed info on arguments
-        window.c.pp = function prettyPrint(){
-            console.dir.apply(console, arguments);
-        };
+    // print detailed info on arguments
+    window.c.pp = function prettyPrint(){
+        console.dir.apply(console, arguments);
+    };
 
-        // we all love old console.log
-        window.c.log = function consoleLog(){
-            var passedMessage = Array.prototype.slice.call(arguments).join(' , '),
-                message = moment().format('H:mm:ss') + ': '+passedMessage;
-            console.log.call(console, message);
-        };
+    // we all love old console.log
+    window.c.log = function consoleLog(){
+        var passedMessage = Array.prototype.slice.call(arguments).join(' , '),
+            message = moment().format('H:mm:ss') + ': '+passedMessage;
+        console.log.call(console, message);
+    };
 
-
+    $('document').ready(function(){
     });
 })(jQuery);

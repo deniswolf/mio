b4ad08090f4f38abd1ffe3158385e8dcccfaf14e
Denis Wolf <dear.mr.dw@gmail.com>
2013-02-10 21:53:35 +0200

    Click button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,9 @@
 (function($){
     $('document').ready(function(){
     // javascript for index page
+        $('#clickOk').on('click', function(){
+            c.log("user clicked button");
+        });
 
 
         c.loaded('index.js');

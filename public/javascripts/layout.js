5693932ab14487c864631208dc33bd902cc118b0
Denis Wolf <dear.mr.dw@gmail.com>
2013-02-17 02:18:47 +0200

    FB auth & browser: fix stupid hash - FB's bug

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -9,6 +9,25 @@
         if (location.hash === '') { return 'unknown'; }
     };
 
+
+    // fix for FB bug: https://github.com/jaredhanson/passport-facebook/issues/12#issuecomment-5913711
+    if (window.location.hash && window.location.hash === "#_=_") {
+        // If you are not using Modernizr, then the alternative is:
+        if (window.history && history.pushState) {
+            window.history.pushState("", document.title, window.location.pathname);
+        } else {
+            // Prevent scrolling by storing the page's current scroll offset
+            var scroll = {
+                top: document.body.scrollTop,
+                left: document.body.scrollLeft
+            };
+            window.location.hash = "";
+            // Restore the scroll offset, should be flicker free
+            document.body.scrollTop = scroll.top;
+            document.body.scrollLeft = scroll.left;
+        }
+    }
+
     //  PRETTY PRINT
     //  logging functions wrapped for use in dev mode only
     window.c = {};

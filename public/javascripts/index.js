b08a38996675602b8b0f5b9106f2fd2df04c0c8f
phenchanter <phenchanter@gmail.com>
2013-03-19 11:29:27 +0400

    added tutorial, style change

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,7 +12,25 @@
             window.location.href = '/auth/facebook';
         });
 
+        //tutorial start (start only for not a app user)
+        var t = setTimeout(startTutotial,1000);
 
         c.loaded('index.js');
     });
 })(jQuery);
+var index=0,introText;
+var startTutotial = function(){
+    introText = "Small tutorial description text."
+    introText = introText.split(' ');
+    $('#tutorial').show();
+    readText();
+};
+var readText = function(){
+    $('#tutorial span:last').css({opacity:1});
+    $('#tutorial').append('<span> '+introText[index]+'</span>');
+    if(index == introText.length){
+        return;
+    }
+    index++;
+    setTimeout(readText,100)
+}
